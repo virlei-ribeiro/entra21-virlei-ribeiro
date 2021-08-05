49fa33e8e5c38e57c9c57e03d457f5982968dc2c
VIRLEI RIBEIRO DE LIMA <virlei.lima@alunos.sc.senac.br>
2021-08-05 09:27:01 -0300

    atual node

---
+++ aula20PraticandoNpm/indexCowsay.js
@@ -0,0 +1,6 @@
+const cowsay = require ("cowsay");
+console.log(cowsay.say({ // mostrou e chamou o (cowsay.say)
+    text: "ola mundo",
+    e: "oO", // VIRGULA não pode ser ; !!
+    T: "U" // T MAIÚSCULO 
+}));
